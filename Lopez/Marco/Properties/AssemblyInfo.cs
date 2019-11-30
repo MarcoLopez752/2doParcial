2dc9a17dfa4bfb21d7c6eb1b44455b5d794e45b9
Marco Antonio Lopez Guevara <marco752slayer@gmail.com>
2019-11-30 10:08:54 -0400

    Conclusion

---
+++ Lopez/Marco/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Marco")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Marco")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f61bc0ba-7947-425c-9f1a-0e91a190a15a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lopez/Marco/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Biblioteca;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Marco
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSave()
+        {
+            //Organizar
+            Person Cliente = new Person("Pedro", 500);
+            double valoresperado = 600;
+
+            //Actuar
+            Cliente.Save(100);
+            double valorobtenido = Cliente.SaveMoney;
+
+            //Afirmar
+
+            Assert.AreEqual(valoresperado, valorobtenido);
+        }
+
+        [TestMethod]
+        public void TestSpend()
+        {
+            //Organizar
+            Person Cliente = new Person("Luana", 1000);
+            double valoresperado = 800;
+
+            //Actuar
+            Cliente.Spend(200);
+            double valorobtenido = Cliente.SaveMoney;
+
+            //Afirmar
+            Assert.AreEqual(valoresperado, valorobtenido);
+        }
+
+
+
+    }
+    
+        
+    
+
+
+
+}
